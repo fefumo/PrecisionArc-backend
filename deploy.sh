1dc2cc449a8538d36c9e5627026346f5ce79e92b
fedos <fedrodj@gmail.com>
2025-05-10 15:46:23 +0300

    switch to gradle

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 WILDFLY_HOME=~/wildfly/wildfly-preview-26.1.3.Final
 
 echo "Building project..."
-if mvn clean install; then
+if gradle build; then
     echo "Build successful. Deploying to WildFly..."
-    cp ./target/PrecisionArc.war $WILDFLY_HOME/standalone/deployments/
+    cp ./build/libs/PrecisionArc.war $WILDFLY_HOME/standalone/deployments/
 else
     echo "Build failed. Check errors above."
     exit 1
